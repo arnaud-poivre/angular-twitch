ad8bc62417403f6b6a28b945a2d0b7cb0b93c8dc
Arnaud <arnaudpoivre@gmail.com>
2023-04-12 09:52:21 +0200

    change url for stream player

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -13,9 +13,9 @@ export const environment = {
   production: true,
   api: {...apiConfig, ...oAuthConfig},
   twitch: {
-    streamPlayerUrl: 'https://player.twitch.tv/?parent=valarep-2023.netlify.app&channel=',
-    clipPlayerUrl: 'https://clips.twitch.tv/embed?parent=valarep-2023.netlify.app&clip=',
-    videoPlayerUrl: 'https://videos.twitch.tv/embed?parent=valarep-2023.netlify.app&video=',
-    chatUrl: 'https://www.twitch.tv/embed/[USERNAME]/chat?parent=valarep-2023.netlify.app',
+    streamPlayerUrl: 'https://player.twitch.tv/?parent=valarep-twitch-clone.netlify.app&channel=',
+    clipPlayerUrl: 'https://clips.twitch.tv/embed?parent=valarep-twitch-clone.netlify.app&clip=',
+    videoPlayerUrl: 'https://videos.twitch.tv/embed?parent=valarep-twitch-clone.netlify.app&video=',
+    chatUrl: 'https://www.twitch.tv/embed/[USERNAME]/chat?parent=valarep-twitch-clone.netlify.app',
   }
 };
